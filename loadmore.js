eb43836abadfe928c24368477721af2e0040a5b3
mattheweq <matthew@olk1.com>
2023-09-05 20:57:15 +0100

    configure image preload

--- loadmore.js
+++ loadmore.js
@@ -1,60 +1,95 @@
-let items = []; 
-const itemsPerPage = 5; 
+let items = [];
+const itemsPerPage = 5;
 let currentPage = 0;
 
 const loadMoreBtn = document.getElementById("loadMoreBtn");
+const loadingMessage = document.getElementById("loadingMessage");
 loadMoreBtn.addEventListener("click", loadItems);
 
+function preloadImages(urls, callback) {
+    var loadedImages = 0;
+    var totalImages = urls.length;
+
+    urls.forEach(function(url) {
+        var img = new Image();
+        img.onload = function() {
+            loadedImages++;
+            if (loadedImages === totalImages) {
+                callback();
+            }
+        };
+        img.src = url;
+    });
+}
+
+function showLoadingMessage() {
+    loadingMessage.style.display = "block";
+}
+
+function hideLoadingMessage() {
+    loadingMessage.style.display = "none";
+}
+
+// Function to load items after preloading images
 function loadItems() {
-  const itemContainer = document.getElementById("itemContainer");
 
-  const startIndex = currentPage * itemsPerPage;
-  const endIndex = startIndex + itemsPerPage;
+    showLoadingMessage();
 
-  // Slice the items array to get the items for the current page
-  const itemsToDisplay = items.slice(startIndex, endIndex);
+    const itemContainer = document.getElementById("itemContainer");
 
-  // Generate HTML for the items and append to the container
-  let itemsHTML = "";
-  for (const item of itemsToDisplay) {
-    itemsHTML += `<a href="${item?.spotifyUrl}">
-    <div class="item-wrapper">
-      <img src="${item?.artwork}" alt="${item?.title}" />
-      <div class="meta">
-        <p class="title">${item?.title}</p>
-        <p class="year">(${item?.year})</p>
-      </div>
-    </div></a>`;
-  }
-  itemContainer.innerHTML += itemsHTML;
+    const startIndex = currentPage * itemsPerPage;
+    const endIndex = startIndex + itemsPerPage;
 
+    // Slice the items array to get the items for the current page
+    const itemsToDisplay = items.slice(startIndex, endIndex);
 
-  // Scroll to the position before loading new items
-  // Calculate the position of the button
-  const loadMoreBtnPosition = loadMoreBtn.getBoundingClientRect().top + window.scrollY;
+    // Generate HTML for the items and append to the container
+    let itemsHTML = "";
+    for (const item of itemsToDisplay) {
+        itemsHTML += `<a href="${item?.spotifyUrl}">
+        <div class="item-wrapper">
+          <img src="${item?.artwork}" alt="${item?.title}" />
+          <div class="meta">
+            <p class="title">${item?.title}</p>
+            <p class="year">(${item?.year})</p>
+          </div>
+        </div></a>`;
+    }
 
-  // Scroll to the position of the "Load More" button after loading items
-  setTimeout(() => {
-    loadMoreBtn.scrollIntoView({ behavior: "smooth" });
+    // Add image URLs to preload
+    const imageUrlsToPreload = itemsToDisplay.map(item => item.artwork);
 
-    window.scrollTo({ top: loadMoreBtnPosition, behavior: "smooth" });
-  }, 100);
-  
+    // Preload images and then update the container
+    preloadImages(imageUrlsToPreload, function() {
+        itemContainer.innerHTML += itemsHTML;
+        hideLoadingMessage();
 
-  // 
-  currentPage++;
+        // Scroll to the position before loading new items
+        // Calculate the position of the button
+        const loadMoreBtnPosition = loadMoreBtn.getBoundingClientRect().top + window.scrollY;
 
-  // Check if there are more items to load
-  if (currentPage * itemsPerPage >= items.length) {
-    document.getElementById("loadMoreBtn").style.display = "none";
-  }
+        // Scroll to the position of the "Load More" button after loading items
+        setTimeout(() => {
+            loadMoreBtn.scrollIntoView({ behavior: "smooth" });
+
+            window.scrollTo({ top: loadMoreBtnPosition, behavior: "smooth" });
+        }, 100);
+
+        // Increment the current page
+        currentPage++;
+
+        // Check if there are more items to load
+        if (currentPage * itemsPerPage >= items.length) {
+            document.getElementById("loadMoreBtn").style.display = "none";
+        }
+    });
 }
 
 // Initial data load on page load
-fetch("data.json")
-  .then(response => response.json())
-  .then(data => {
-    console.log(`%c ${data.status}`, 'color: white; background-color: green; padding: 10px;')
-    items = data.music.releases.reverse(); // Update the global items array with new data
-    loadItems(); // Initial load of items
-  });
+fetch("https://raw.githubusercontent.com/mvmdiscography/mvmdiscography.github.io/main/data.json")
+    .then(response => response.json())
+    .then(data => {
+        console.log(`%c ${data.status}`, 'color: white; background-color: green; padding: 10px;');
+        items = data.music.releases.reverse(); // Update the global items array with new data
+        loadItems(); // Initial load of items
+    });
